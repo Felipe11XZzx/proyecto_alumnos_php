6ab6098f1beb0c81407668931e6bb676af9e1683
Felipe Ortiz <ortiz15243u@gmail.com>
2025-03-14 10:43:07 +0100

    Implementado documentacion Swagger y API con Postman

---
+++ app/Http/Controllers/AlumnoApiController.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Requests\StoreAlumnoRequest;
+use App\Http\Resources\AlumnoCollection;
+use App\Http\Resources\AlumnoResource;
+use App\Models\Alumno;
+use Illuminate\Http\Request;
+use function Pest\Laravel\json;
+
+// URL de la documentacion de la API: http://localhost:8000/api/documentation
+/**
+ * @OA\Info(
+ *      title="API para consultar alumnos de mi centro",
+ *      version="2.0.0",
+ *      description="Esta api permite interactuar con los alumnos de bd del instituto",
+ *      @OA\Contact(
+ *          name="Felipe Ortiz",
+ *          email="felipe1111@gmail.com",
+ *      ),
+ *      @OA\License(
+ *          name="MIT",
+ *          url="https://opensource.org/license/mit",
+ *      )
+ * )
+ */
+class AlumnoApiController extends Controller
+{
+    /**
+     * @OA\Get(
+     *      path="/api/alumnos",
+     *      operationId="getAllStudents",
+     *      tags={"Alumnos"},
+     *      summary="Obtener todos los alumnos",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Éxito",
+     *      )
+     * )
+     */
+    public function index(Request $request)
+    {
+        return new AlumnoCollection(Alumno::all());
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAlumnoRequest $request)
+    {
+        $datos = $request->input("data.attributes");
+        $alumno = new Alumno($datos);
+        $alumno->save();
+        return new AlumnoResource($alumno);
+        //
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
